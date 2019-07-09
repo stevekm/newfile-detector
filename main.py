bc416f9ab8bb18ef679c657f5c25875315889d2e
Stephen Kelly <stevekm@users.noreply.github.com>
2019-07-09 13:39:45 -0400

    add files to database

--- main.py
+++ main.py
@@ -1,21 +1,44 @@
-# Django specific settings
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+import glob
 
+# Initailize Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 # Ensure settings are read
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
-
-# Your application specific imports
 from db.models import User
+from db.models import File
 
+def get_file_list(files_dir = "files"):
+    glob_pattern = os.path.join(files_dir, '*')
+    files = glob.glob(glob_pattern)
+    return(files)
 
-#Add user
-user = User(name="masnun", email="masnun@gmail.com")
-user.save()
+def add_file(filename):
+    """
+    Add the file to the database if it does not already exist
+    Skips creation for files that already exist, based on provided filename
+    """
+    instance, created = File.objects.get_or_create(filename = filename)
+    return(created)
 
-# Application logic
-first_user = User.objects.all()[0]
+def file_in_database(filename):
+    """
+    Checks if a file is already in the database
+    """
+    return(File.objects.filter(filename = filename).exists())
 
-print(first_user.name)
-print(first_user.email)
+def main():
+    files = get_file_list()
+    print("Found files:\n{0}\n".format(files))
+    for file in files:
+        is_in_db = file_in_database(filename = file)
+        print("File {0} already in database: {1}".format(file, is_in_db))
+        created = add_file(filename = file)
+        if created:
+            print("Added file {0} to database".format(file))
+        else:
+            print("Did not add file {0} to database".format(file))
+
+if __name__ == '__main__':
+    main()
